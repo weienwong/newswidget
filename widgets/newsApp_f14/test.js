15596fe10117a91be817b41f850b439817ec1d65
Beccaneer <thomson.rebecca@gmail.com>
2014-11-25 17:03:13 -0500

    Update test.js

--- widgets/newsApp_f14/test.js
+++ widgets/newsApp_f14/test.js
@@ -5,12 +5,12 @@ $.ajax({
     url : 'http://uwchevron.wordpress.com/feed/',
     dataType: 'xml',
     error : function (xml){
-        alert("did not work");
+        alert("grabbing data did not work");
     },
     success : function (xml){ 
         $(xml).find("item").each(function() {
            var title = $(this).find("title").text();
-           alert("worked");
+           alert("grabbing data worked");
       });
 }
 });
