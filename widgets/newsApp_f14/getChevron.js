bf7b7f4b7df4cfab77af607f8e54cf6f8afb763a
Beccaneer <thomson.rebecca@gmail.com>
2014-11-28 13:25:11 -0500

    Update getChevron.js

--- widgets/newsApp_f14/getChevron.js
+++ widgets/newsApp_f14/getChevron.js
@@ -1,1 +1,19 @@
 // This is to grab data from the UW Chevron's Twitter
+// not working until can figure out how to use twitter
+
+
+function getChevron(){
+	var TwitterAPIKey = "nugk4CM3hmyMnQWNjnTYSg0H5";
+	
+	 $.ajax({
+     type: "GET",
+     dataType: "json",
+     url: "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=TheUWChevron",
+     error: function(){
+			alert("Failed to retreieve data");
+		},
+     success: function(data){
+        $("#tweet_result").text(data.results[0].text);
+     }
+   });
+}
