4f9e9f6076f0c2f01b5b6c1d7d1f4f1b7e37d5b7
Beccaneer <thomson.rebecca@gmail.com>
2014-11-28 13:57:27 -0500

    Update getChevron.js

--- widgets/newsApp_f14/getChevron.js
+++ widgets/newsApp_f14/getChevron.js
@@ -4,16 +4,17 @@
 
 function getChevron(){
 	var TwitterAPIKey = "nugk4CM3hmyMnQWNjnTYSg0H5";
-	
-	 $.ajax({
+    
+    $.ajax({
      type: "GET",
      dataType: "json",
      url: "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=TheUWChevron?key="+TwitterAPIKey,
      error: function(){
-			alert("Failed to retreieve Twitter data");
+		   alert("Failed to retreieve Twitter data");
 		},
-     success: function(data){
-        $("#tweet_result").text(data.results[0].text);
+     success: function(){
+     	alert("Able to retreieve Twitter data");
+        //$("#tweet_result").text(data.results[0].text);
      }
    });
 }
