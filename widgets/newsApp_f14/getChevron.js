2d489704a590181401a0836146fbb6384d5fbbad
Beccaneer <thomson.rebecca@gmail.com>
2014-11-28 15:41:00 -0500

    Update getChevron.js

--- widgets/newsApp_f14/getChevron.js
+++ widgets/newsApp_f14/getChevron.js
@@ -5,6 +5,8 @@
 function getChevron(){
 	var TwitterAPIKey = "nugk4CM3hmyMnQWNjnTYSg0H5";
 	OAuth.initialize(TwitterAPIKey);
+	
+    	OAuth.redirect('twitter', "callback/url");
     
     	OAuth.callback('twitter')
 	.done(function(result) {
