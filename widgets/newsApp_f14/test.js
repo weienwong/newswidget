d6db0a7c33a91008df775c74a6da4d700d222889
Beccaneer <thomson.rebecca@gmail.com>
2014-11-28 13:13:20 -0500

    Update test.js

--- widgets/newsApp_f14/test.js
+++ widgets/newsApp_f14/test.js
@@ -1,17 +1,17 @@
 function testit(){
--
--$.ajax({ 
--    type: 'GET',
--    url : 'http://localhost:8080/uwchevron.wordpress.com/feed/',
--    dataType: 'xml',
--    error : function (xml){
--        alert("grabbing data did not work");
--    },
--    success : function (xml){ 
--        $(xml).find("item").each(function() {
--           var title = $(this).find("title").text();
--           alert("grabbing data worked");
--        });
--    }
--});
+
+$.ajax({ 
+    type: 'GET',
+    url : 'http://localhost:8080/uwchevron.wordpress.com/feed/',
+    dataType: 'xml',
+    error : function (xml){
+        alert("grabbing data did not work");
+    },
+    success : function (xml){ 
+        $(xml).find("item").each(function() {
+           var title = $(this).find("title").text();
+           alert("grabbing data worked");
+        });
+    }
+});
 }
