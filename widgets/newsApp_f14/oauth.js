8051d814057bdaba03bc2eaafb6e5c3393b28911
Beccaneer <thomson.rebecca@gmail.com>
2014-11-28 14:53:14 -0500

    Update oauth.js

--- widgets/newsApp_f14/oauth.js
+++ widgets/newsApp_f14/oauth.js
@@ -1,6 +1,7 @@
 // THIS IS REQUIRED FOR USING THE TWITTER API
 // Oauth used with javascript, which is not ideal but is allowed through the assignment restrictions
-// 
+// used with permission
+// project site can be found here: https://github.com/oauth-io/oauth-js
 
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = {
