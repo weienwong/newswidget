575a13be8ca65f5a1848996f80b4b54545434474
Beccaneer <thomson.rebecca@gmail.com>
2014-11-28 16:28:14 -0500

    Update getChevron.js

--- widgets/newsApp_f14/getChevron.js
+++ widgets/newsApp_f14/getChevron.js
@@ -1,9 +1,38 @@
 // This is to grab data from the UW Chevron's Twitter
 // not working until can figure out how to use twitter
-
+function handleTweets(tweets){
+    var x = tweets.length;
+    var n = 0;
+    var element = document.getElementById('chevron');
+    var html = '<ul>';
+    while(n < x) {
+      html += '<li>' + tweets[n] + '</li>';
+      var newsPanel = Mustache.to_html(panelTemplate, tweets[n]);
+		$("#newsApp_f14_thirdTab").append(newsPanel);
+      n++;
+    }
+    html += '</ul>';
+    element.innerHTML = html;
+}
 
 function getChevron(){
-	var TwitterAPIKey = "nugk4CM3hmyMnQWNjnTYSg0H5";
+	
+	//attemtping to use Twitter Fetcher
+	
+	var chevronfeed {
+  		"id": '347099293930377217',
+  		"domId": 'chevron',
+  		"maxTweets": 5,
+  		"enableLinks": true,
+  		"showUser": true,
+  		"showTime": true,
+  		"lang": 'en',
+  		"customCallback": handleTweets
+	};
+	twitterFetcher.fetch(chevronfeed);
+	
+	
+	/*var TwitterAPIKey = "nugk4CM3hmyMnQWNjnTYSg0H5";
 	OAuth.initialize(TwitterAPIKey);
 	
     
@@ -23,5 +52,5 @@ function getChevron(){
 	.fail(function (err) {
   		console.log("Failure fetching Twitter UW Chevron data: OAuth.redirect");
   		console.log(err);
-	});
+	});*/
 }
