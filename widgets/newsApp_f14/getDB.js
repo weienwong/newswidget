ade0a96dc121d09ad645c63dae7813abd7b2c8ee
Beccaneer <thomson.rebecca@gmail.com>
2014-11-29 16:51:34 -0500

    Update getDB.js

--- widgets/newsApp_f14/getDB.js
+++ widgets/newsApp_f14/getDB.js
@@ -19,7 +19,7 @@ function handleTweets(tweets){
       	subString[1] = subString[1].replace("</p>", "");
       
      	var newNews = new newsItem(subString[0], subString[1], "http://bulletin.uwaterloo.ca/", "@UWBulletin");
-     	var panelTemplate = "<div class=\"panel panel-default\"><div class=\"panel-body\"><b>"+newNews.article+"</b><br><br><div class=\"publishInfo\">Tweeted "+newNews.date+"<br><a href=\"https://twitter.com/uwdailybulletin\">"+newNews.author+"</a></div><br></div></div></div>"
+     	var panelTemplate = "<div class=\"panel panel-default\"><div class=\"panel-body\"><b>"+newNews.article+"</b><br><br><div class=\"publishInfo\">"+newNews.date+"<br><a href=\"https://twitter.com/uwdailybulletin\">"+newNews.author+"</a></div><br></div></div></div>"
       	var newsPanel = Mustache.to_html(panelTemplate);
 		    $("#newsApp_f14_fifthTab").append(newsPanel);
      
