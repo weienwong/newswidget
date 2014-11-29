d6af28847baa46fe8aa73240f0e9ee1f04a049bd
Beccaneer <thomson.rebecca@gmail.com>
2014-11-29 16:48:29 -0500

    Update getUWtwitter.js

--- widgets/newsApp_f14/getUWtwitter.js
+++ widgets/newsApp_f14/getUWtwitter.js
@@ -15,15 +15,17 @@ function handleTweets(tweets){
   	// ["<p class="tweet">Look for new articles soon!", "Posted on 14 Nov</p>"]
   
   	subString[0] = subString[0].substr(17); // to remove the "P class tweet" tag
-	  subString[1] = subString[1].substr(10, 6); // to remove "posted on" and "</p>"
+	//subString[1] = subString[1].substr(10, 6); // to remove "posted on" and "</p>"
+        subString[1] = subString[1].replace("Posted on", "Tweeted");
+      	subString[1] = subString[1].replace("</p>", "");
       
-    if (subString[1] === "hours "){
-      	subString[1] = "hours ago";
-    }
+    	if (subString[1] === "hours "){
+      		subString[1] = "hours ago";
+    	}
       
-    var newNews = new newsItem(subString[0], subString[1], "http://uwimprint.ca/", "@UWaterloo");
-    var panelTemplate = "<div class=\"panel panel-default\"><div class=\"panel-body\"><b>"+newNews.article+"</b><br><br><div class=\"publishInfo\">Tweeted "+newNews.date+"<br><a href=\"https://twitter.com/UWaterloo\">"+newNews.author+"</a></div><br></div></div></div>"
-    var newsPanel = Mustache.to_html(panelTemplate);
+    	var newNews = new newsItem(subString[0], subString[1], "http://uwimprint.ca/", "@UWaterloo");
+    	var panelTemplate = "<div class=\"panel panel-default\"><div class=\"panel-body\"><b>"+newNews.article+"</b><br><br><div class=\"publishInfo\">Tweeted "+newNews.date+"<br><a href=\"https://twitter.com/UWaterloo\">"+newNews.author+"</a></div><br></div></div></div>"
+    	var newsPanel = Mustache.to_html(panelTemplate);
 		$("#newsApp_f14_sixthTab").append(newsPanel);
      
     }
