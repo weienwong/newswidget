f5ac4686305fb98b80d1f5d45c3d5241d6932815
Beccaneer <thomson.rebecca@gmail.com>
2014-11-29 13:24:43 -0500

    Update newsApp_f14.js

--- widgets/newsApp_f14/newsApp_f14.js
+++ widgets/newsApp_f14/newsApp_f14.js
@@ -44,7 +44,7 @@ function showTabs(){
 	
 	// makes function calls to grab and display news content from sources
 	getNews();
-	//getImprint();
+	getImprint();
 	getChevron();
 	getSubreddit();
 	//testit();
