b046bffa93389dcc630fa2a0715f1a814c5e294c
Beccaneer <thomson.rebecca@gmail.com>
2014-11-29 13:30:59 -0500

    Update getImprint.js

--- widgets/newsApp_f14/getImprint.js
+++ widgets/newsApp_f14/getImprint.js
@@ -18,7 +18,7 @@ function handleTweets(tweets){
       
       
      	var newNews = new newsItem(subString[0], subString[1], "http://uwimprint.ca/", "Imprint");
-     	var panelTemplate = "<div class=\"panel panel-default\"><div class=\"panel-body\"><b>"+newNews.article+"</b><br><br><div class=\"publishInfo\">Published on "+newNews.date+"<br> by: <a href=\"http://uwimprint.ca\">"+newNews.author+"</a></div><br></div></div></div>"
+     	var panelTemplate = "<div class=\"panel panel-default\"><div class=\"panel-body\"><b>"+newNews.article+"</b><br><br><div class=\"publishInfo\">Tweeted "+newNews.date+"<br> by: <a href=\"http://uwimprint.ca\">"+newNews.author+"</a></div><br></div></div></div>"
       	var newsPanel = Mustache.to_html(panelTemplate);
 		$("#newsApp_f14_thirdTab").append(newsPanel);
      
