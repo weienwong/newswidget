fc7c9c630986ba1cf80ddcbf142be280cf74b519
Beccaneer <thomson.rebecca@gmail.com>
2014-11-29 12:53:45 -0500

    Update getChevron.js

--- widgets/newsApp_f14/getChevron.js
+++ widgets/newsApp_f14/getChevron.js
@@ -11,7 +11,7 @@ function handleTweets(tweets){
    
     for (var i = 0; i < tweetNum; i++){
       var newsList = breakUpNews(tweets[i]);
-      console.log(newsList);
+      console.log(newsList[0]);
       var newNews = newsItem(newsList[0], newsList[1], "Chevron", "http://uwchevron.wordpress.com/");
      var panelTemplate = "<div class=\"panel panel-default\"><div class=\"panel-body\"><a target=\"_blank\" href="+newNews.url+"><b>"+newNews.article+"</b></a><br><br><div class=\"publishInfo\">Posted by: "+newNews.author+"</div></div></div>"
       var newsPanel = Mustache.to_html(panelTemplate);
