39e7ec8748e91dbccc91300d59285959a3c9c90d
Beccaneer <thomson.rebecca@gmail.com>
2014-11-29 16:05:00 -0500

    Update getChevron.js

--- widgets/newsApp_f14/getChevron.js
+++ widgets/newsApp_f14/getChevron.js
@@ -17,8 +17,8 @@ function handleTweets(tweets){
 	subString[1] = subString[1].substr(10, 6); // to remove "posted on" and "</p>"
       
       
-     	var newNews = new newsItem(subString[0], subString[1], "http://uwchevron.wordpress.com/", "The UW Chevron");
-     	var panelTemplate = "<div class=\"panel panel-default\"><div class=\"panel-body\"><b>"+newNews.article+"</b><br><br><div class=\"publishInfo\">Tweeted "+newNews.date+"<br> by: <a href=\"http://uwchevron.wordpress.com\">"+newNews.author+"</a></div><br></div></div></div>"
+     	var newNews = new newsItem(subString[0], subString[1], "http://uwchevron.wordpress.com/", "@TheUWChevron");
+     	var panelTemplate = "<div class=\"panel panel-default\"><div class=\"panel-body\"><b>"+newNews.article+"</b><br><br><div class=\"publishInfo\">Tweeted "+newNews.date+"<br> <a href=\"https://twitter.com/uwchevron\">"+newNews.author+"</a></div><br></div></div></div>"
       	var newsPanel = Mustache.to_html(panelTemplate);
 		$("#newsApp_f14_fourthTab").append(newsPanel);
      
